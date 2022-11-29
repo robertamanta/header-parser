b2aeb4ce80d1574fb07b2c2be2d336957cf09e06
robertamanta <116081834+robertamanta@users.noreply.github.com>
2022-11-29 21:41:08 +0200

    first_commit

---
+++ server.js
@@ -0,0 +1,30 @@
+
+// init project
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const requestIP = require('request-ip');
+const cors = require('cors');
+
+
+app.use(cors({ optionsSuccessStatus: 200 })); 
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/whoami', function (req, res) {
+  let ipAddress =  req.ip;//requestIP.getClientIp(req);
+  let language =  req.headers["accept-language"] ;
+  let software =  req.header("user-agent");
+ 
+  res.json({ ipaddress: ipAddress, language: language ,software: software });
+});
+
+
+
+// listen for requests
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
